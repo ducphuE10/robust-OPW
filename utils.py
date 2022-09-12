6ad9851d77e07342fe2228b33c2c0e2502573a9c
ducphu247 <nguyenducphu201101@gmail.com>
2022-09-12 14:55:54 +0700

    add viz

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize(data,trend,threshold = None, fig_size = (30,10)):
+
+def visualize(data,trend,threshold = None, fig_size = (30,20)):
     '''
     data: input data : numpy array
     trend: result of apply trend filter : numpy array
@@ -9,12 +10,18 @@ def visualize(data,trend,threshold = None, fig_size = (30,10)):
     fig_size: figure size 
     '''
     fig = plt.figure(figsize=fig_size)
+    #TREND & DATA
+    # plt.subplot(2,1,1)
     residual = data - trend
     plt.plot(np.arange(data.shape[0]),data,label = 'data')
     plt.plot(np.arange(data.shape[0]),trend, label = 'trend')
     if threshold:
         idx = np.where(np.abs(residual) > threshold)[0]
         plt.scatter(idx, data[idx],c = 'red')
+
+    #RESIDUAL
+    # plt.subplot(2,1,2)
+    # plt.plot(np.arange(data.shape[0]),data - trend)
 
     plt.legend(loc = 'upper left')
     plt.show()
